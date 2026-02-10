c964dad84ebaa68b63ccc5cc3d199ca228c200b5
Micro-Prog <sajjadsttr01@gmail.com>
2026-02-11 00:34:22 +0330

    modified .env

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,7 @@ import './index.css';
 import './styles/styles.css';
 import App from './App.jsx';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
